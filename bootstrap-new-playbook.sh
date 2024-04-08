b8249a72fcb35746a47d6a39e7f9562f6fbd1c26
Andy Godish <agodish18@gmail.com>
2024-04-07 22:26:05 -0600

    simplifed workflow actions, updated new-playbook script to add in a simple main.yaml file

--- bootstrap-new-playbook.sh
+++ bootstrap-new-playbook.sh
@@ -9,6 +9,11 @@ else
   mkdir $ANSIBLE_PLAYBOOK_NAME/roles
   mkdir $ANSIBLE_PLAYBOOK_NAME/collections
 
+  echo "---" > $ANSIBLE_PLAYBOOK_NAME/main.yaml
+  echo "- name: $ANSIBLE_PLAYBOOK_NAME" >> $ANSIBLE_PLAYBOOK_NAME/main.yaml
+  echo "  hosts: all" >> $ANSIBLE_PLAYBOOK_NAME/main.yaml
+  echo "  become: true" >> $ANSIBLE_PLAYBOOK_NAME/main.yaml
+
   cp /config/ansible.cfg /app/$ANSIBLE_PLAYBOOK_NAME
   cp /config/hosts.ini.example /app/$ANSIBLE_PLAYBOOK_NAME/inventory/hosts.ini
 fi
